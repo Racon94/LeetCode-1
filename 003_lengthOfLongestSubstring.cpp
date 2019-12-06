8b58966dfdf4aafa46d527cf229b48c61170c8d9
severin zhong <severinzhong@gmail.com>
2019-12-06 22:05:34 +0800

    #leetcode 3 length for longest substring

---
+++ 003_lengthOfLongestSubstring.cpp
@@ -0,0 +1,17 @@
+#include "Solution.h"
+
+int Solution::lengthOfLongestSubstring(string s) {
+	int ch[129] = {};
+	int ret = 0,tmp = 0;
+	for (int i = 0; i < s.size(); i++) {
+		if (ch[s[i]]) {
+			while (tmp<i && ch[s[i]]) {
+				ch[s[tmp]] --;
+				tmp ++;
+			} 
+		}
+		ret = ret > i - tmp+1 ? ret : i - tmp + 1;
+		ch[s[i]] ++;
+	}
+	return ret; 
+}
